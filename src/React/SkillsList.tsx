c5162516924426f245d79458995dac1eb9a5e5e4
Al-Amin Likhon <alaminlikhon283@gmail.com>
2026-01-31 17:34:33 +0600

    deployment fix

--- src/React/SkillsList.tsx
+++ src/React/SkillsList.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const CategoryIcons: Record<string, JSX.Element> = {
+const CategoryIcons: Record<string, React.ReactElement> = {
   "Video editing": (
     <svg
       xmlns="http://www.w3.org/2000/svg"
